b3ac52ced2738fb110cda65e8ccea0872e34e85f
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-02-11 22:41:32 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,14 +7,16 @@ let count = 0
 console.log(saveEntry)
 function increment(){
     count = count + 1
-    countEl.innerText = count
+    countEl.textContent = count
     // alert("clicked")
 }
 function save(){
-    console.log(count)
+    // console.log(count)
     let countStr = count+" - "
-    saveEntry.innerText += countStr
-    console.log(countEl)
+    saveEntry.textContent += countStr
+    // console.log(countEl)
+    count = 0
+    countEl.textContent = 0
 }
 
 let msg = "you have three new notification "
